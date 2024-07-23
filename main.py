5240a89467fc2c73e8e1cf7dce66058914f84ea6
Pal patel <115624654+PalProdution@users.noreply.github.com>
2024-07-23 16:10:48 +0530

    Update main.py

--- main.py
+++ main.py
@@ -27,4 +27,33 @@ while True:
     imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)  # Convert to rgb
 
     # Collection of gesture information
-    results = hands.process(imgRGB)  # completes the image processing.  
+    results = hands.process(imgRGB)  # completes the image processing. 
+    lmList = []  # empty list
+    if results.multi_hand_landmarks:  # list of all hands detected.
+        # By accessing the list, we can get the information of each hand's corresponding flag bit
+        for handlandmark in results.multi_hand_landmarks:
+            for id, lm in enumerate(handlandmark.landmark):  # adding counter and returning it
+                # Get finger joint points
+                h, w, _ = img.shape
+                cx, cy = int(lm.x * w), int(lm.y * h)
+                lmList.append([id, cx, cy])  # adding to the empty list 'lmList'
+            mpDraw.draw_landmarks(img, handlandmark, mpHands.HAND_CONNECTIONS)
+
+    if lmList != []:
+        # getting the value at a point
+        # x      #y
+        x1, y1 = lmList[4][1], lmList[4][2]  # thumb
+        x2, y2 = lmList[8][1], lmList[8][2]  # index finger
+        # creating circle at the tips of thumb and index finger
+        cv2.circle(img, (x1, y1), 13, (255, 0, 0), cv2.FILLED)  # image #fingers #radius #rgb
+        cv2.circle(img, (x2, y2), 13, (255, 0, 0), cv2.FILLED)  # image #fingers #radius #rgb
+        cv2.line(img, (x1, y1), (x2, y2), (255, 0, 0), 3)  # create a line b/w tips of index finger and thumb
+
+        length = hypot(x2 - x1, y2 - y1)  # distance b/w tips using hypotenuse
+        # from numpy we find our length,by converting hand range in terms of volume range ie b/w -63.5 to 0
+        vol = np.interp(length, [30, 350], [volMin, volMax])
+        volbar = np.interp(length, [30, 350], [400, 150])
+        volper = np.interp(length, [30, 350], [0, 100])
+
+        print(vol, int(length))
+        volume.SetMasterVolumeLevel(vol, None)
